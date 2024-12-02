1d61cd20becafd1212f9f0a6c6e1eb9e53d815c5
Madhurima <madhurimaofficial7>
2024-12-02 09:19:50 +0530

    first commit

---
+++ src/test/java/in/delite/spring_a1/SpringA1ApplicationTests.java
@@ -0,0 +1,13 @@
+package in.delite.spring_a1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringA1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
